09da7769089e9e0b1e49243016f9ec0c8642b2aa
pornpun_pw <6310210272@psu.ac.th>
2023-10-15 17:48:13 +0700

    back succuss

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -8,24 +8,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.demo.model.Faculty;
 import com.example.demo.model.Location;
-import com.example.demo.repository.FacultyRepository;
 import com.example.demo.repository.LocationRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class LocationController {
 	
 	@Autowired
